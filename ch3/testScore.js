32742ca776420be156db903ed975282525b53231
Kyle Givler <xvegan88x@gmail.com>
2021-08-22 14:04:41 -0400

    Chapter 3

---
+++ ch3/testScore.js
@@ -0,0 +1,18 @@
+let total = 0;
+let count = 0;
+let score = 0;
+
+do {
+    score = parseInt(prompt("Enter a test score, or enter -1 to end scores.", -1));
+
+    if (score >= 0 && score <= 100) {
+        total += score;
+        count++;
+        document.write(`<p>Score ${count}: ${score}</p>`);
+    } else if (score != -1) {
+        alert("Score must be a valid number from 0 through 100.");
+    }
+} while(score != -1)
+
+const average = parseInt(total / count);
+document.write(`<p>Average score is ${average}</p>`);
